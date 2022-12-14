b96a2228c94e90e43d2a23f23f0d338652633580
Mystilund <development.xplx@gmail.com>
2022-12-14 22:46:33 +0100

    fix: remove useless files and edit build-dev

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,4 +1,4 @@
 npx nx run card-generator:build && \
   npx nx run proxy:build:development && \
-  npx nx run lundprod:build:development && \
+  npx nx run lundprod:build:production && \
   npx nx run bot:build:development
