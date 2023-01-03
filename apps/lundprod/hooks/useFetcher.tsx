d95e6aced26aa45b1a0050d18cfdf2a93e7d87e7
Mystilund <development.xplx@gmail.com>
2023-01-03 15:52:35 +0100

    fix: loop of fetch

--- apps/lundprod/hooks/useFetcher.tsx
+++ apps/lundprod/hooks/useFetcher.tsx
@@ -1,60 +1,67 @@
 import { useToast, Text } from '@chakra-ui/react';
 import Link from 'next/link';
+import { useMemo } from 'react';
 
 export const useFetcher = () => {
   const toast = useToast();
 
-  return (
-    url: string,
-    queryParams?: Record<string, unknown | unknown[]>,
-    init?: RequestInit
-  ) => {
-    const query = new URLSearchParams();
+  const fetcher = useMemo(
+    () =>
+      (
+        url: string,
+        queryParams?: Record<string, unknown | unknown[]>,
+        init?: RequestInit
+      ) => {
+        const query = new URLSearchParams();
 
-    if (queryParams) {
-      Object.entries(queryParams).forEach(([key, value]) => {
-        if (Array.isArray(value)) {
-          value.forEach((v) => query.append(key, String(v)));
-        } else {
-          query.append(key, String(value));
+        if (queryParams) {
+          Object.entries(queryParams).forEach(([key, value]) => {
+            if (Array.isArray(value)) {
+              value.forEach((v) => query.append(key, String(v)));
+            } else {
+              query.append(key, String(value));
+            }
+          });
         }
-      });
-    }
 
-    return fetch(`${url}?${query.toString()}`, init).then((response) => {
-      if (response.ok) {
-        return response.json();
-      }
+        return fetch(`${url}?${query.toString()}`, init).then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
 
-      if (response.status === 401) {
-        toast({
-          title: 'Erreur',
-          description: (
-            <Text>
-              Vous n&apos;êtes plus authentifié. Rafraichissez votre page ou{' '}
-              <Link href="/auth/signin">reconnectez vous</Link>
-            </Text>
-          ),
-          status: 'error',
-          duration: 9000,
-          isClosable: true,
-          position: 'bottom-left',
+          if (response.status === 401) {
+            toast({
+              title: 'Erreur',
+              description: (
+                <Text>
+                  Vous n&apos;êtes plus authentifié. Rafraichissez votre page ou{' '}
+                  <Link href="/auth/signin">reconnectez vous</Link>
+                </Text>
+              ),
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+              position: 'bottom-left',
+            });
+          } else {
+            toast({
+              title: 'Erreur',
+              description:
+                "Une erreur inconnue s'est produite. Réessayez ultérieurement et contactez Lund pour remonter le probleme",
+              status: 'error',
+              duration: 9000,
+              isClosable: true,
+              position: 'bottom-left',
+            });
+          }
+
+          return response.text().then((errorTxt) => {
+            throw new Error(`The request failed\n${errorTxt}`);
+          });
         });
-      } else {
-        toast({
-          title: 'Erreur',
-          description:
-            "Une erreur inconnue s'est produite. Réessayez ultérieurement et contactez Lund pour remonter le probleme",
-          status: 'error',
-          duration: 9000,
-          isClosable: true,
-          position: 'bottom-left',
-        });
-      }
+      },
+    [toast]
+  );
 
-      return response.text().then((errorTxt) => {
-        throw new Error(`The request failed\n${errorTxt}`);
-      });
-    });
-  };
+  return fetcher;
 };
